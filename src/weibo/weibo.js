f055fe75f69f872daec53ae2321b1e2b7fd3503b
hudk <hdkwd@126.com>
2018-03-21 08:09:16 +0800

    upd8 add scroll

--- src/weibo/weibo.js
+++ src/weibo/weibo.js
@@ -2,6 +2,7 @@ const puppeteer = require('puppeteer');
 const login = require('./login');
 // create your user file nad userList file!
 const userList = require('./userList');
+const scroll = require('../utils/scroll');
 
 const weibo = async () => {
   const browser = await puppeteer.launch({headless: false});
@@ -23,12 +24,19 @@ const id = userList[0];
   }
 
   await page.waitFor(2000);
-  
+
   try {
+    await scroll(page, async p => {
+      // TODO 在某个时间点之前的都可能
+      let ele = await p.$('ul[group_id="2017年03月"]');
+      console.log(ele);
+      return !!ele;
+    });
+    
     // TODO 对视频处理
-    await page.click('ul[group_id="03月"] img[class="photo_pict"]');
-    await page.waitFor(1000);
-    await page.click('div[node-type="wrapIcon"] a[node-type="maximum"]');
+    // await page.click('ul[group_id="03月"] img[class="photo_pict"]');
+    // await page.waitFor(1000);
+    // await page.click('div[node-type="wrapIcon"] a[node-type="maximum"]');
   } catch (error) {
     
   }
